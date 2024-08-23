851993a6d7997e69d1f37ce2ea702fd6d5e2ae3e
Angel G300 Saucedo <saucedog926@gmail.com>
2024-08-23 14:12:57 -0300

    basic config

---
+++ haxballBot.js
@@ -0,0 +1,29 @@
+const room = HBInit({
+	roomName: "creando un bot",
+	maxPlayers: 16,
+	noPlayer: false,
+    public: true,
+    password: "ofi123",
+    playerName: "AB | BOT DE MIERDA"
+});
+
+room.setDefaultStadium("Big");
+room.setScoreLimit(5);
+room.setTimeLimit(0);
+
+// If there are no admins left in the room give admin to one of the remaining players.
+function updateAdmins() { 
+  // Get all players
+  let players = room.getPlayerList();
+  if ( players.length == 0 ) return; // No players left, do nothing.
+  if ( players.find((player) => player.admin) != null ) return; // There's an admin left so do nothing.
+  room.setPlayerAdmin(players[0].id, true); // Give admin to the first non admin player in the list
+}
+
+room.onPlayerJoin = function(player) {
+  updateAdmins();
+}
+
+room.onPlayerLeave = function(player) {
+  updateAdmins();
+}
